34a7294c9df4f5d603cf31b0b61b0e6ae4ddefd1
Luis <gupiluan@hotmail.com>
2024-12-05 16:35:23 -0600

    pull fix

--- src/main/java/com/mayab/quality/unittest/App.java
+++ src/main/java/com/mayab/quality/unittest/App.java
@@ -14,6 +14,7 @@ class App {
         // User userPrueba = new User("username", "password", "email@email.com");
         // daoUser.save(userPrueba);
         System.out.println(daoUser.findAll());
+        // Hola
 
     }
 }
